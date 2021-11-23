c9028f503d4fe2096e96131573ada324f6513881
KKimos <87573686+KKimos@users.noreply.github.com>
2021-11-23 11:36:10 +0800

    Update ERC9999.sol

--- contracts/ERC9999.sol
+++ contracts/ERC9999.sol
@@ -7,18 +7,46 @@ import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contr
 import "./interface/IERC9999.sol";
 
 /// @title cryptosharing
-/// @author libo
+/// @author Kimos
 /// @notice cryptosharing
+
 contract ERC9999 is IERC9999 , ERC721 {
     
-    /// @dev Mapping from token ID to user address
+    // Mapping from token ID to user address
     mapping(uint256 => address) private _users;
 
-    /// @dev Mapping user address to token count
+    // Mapping user address to token count
     mapping(address => uint256) private _balancesOfUser;
     
+     // Mapping from token ID to approved address
+    mapping(uint256 => address) private _tokenUserApprovals;
+
     constructor(string memory name_, string memory symbol_) ERC721(name_ , symbol_){
         
+    }
+    
+    function approveUser(address to, uint256 tokenId) public virtual  {
+        address owner = ERC721.ownerOf(tokenId);
+        address user = ERC9999.userOf(tokenId);
+        require(to != user, "ERC9999: approval to current user");
+
+        require(
+            _msgSender() == owner || _isApprovedOrUser(_msgSender(), tokenId),
+            "ERC9999: approve caller is not owner nor approved for all"
+        );
+
+        _approveUser(to, tokenId);
+    }
+    
+    function _approveUser(address to, uint256 tokenId) internal virtual {
+        _tokenUserApprovals[tokenId] = to;
+        emit ApprovalUser(ERC721.ownerOf(tokenId), to, tokenId);
+    }
+    
+    function getApprovedUser(uint256 tokenId) public view virtual override returns (address) {
+        require(_exists(tokenId), "ERC9999: approved query for nonexistent token");
+
+        return _tokenUserApprovals[tokenId];
     }
     
     function balanceOfUser(address user) public view virtual override returns (uint256) {
@@ -38,8 +66,8 @@ contract ERC9999 is IERC9999 , ERC721 {
         uint256 tokenId
     ) public virtual override {
         //solhint-disable-next-line max-line-length
-        require(_isApprovedOrUser(_msgSender(), tokenId), "ERC9999: transfer caller is not owner nor approved");
-
+        require(_isApprovedOrUser(_msgSender(), tokenId) || ERC721._isApprovedOrOwner(_msgSender() , tokenId), "ERC9999: transfer caller is not user or owner nor approved");
+        
         _transfer(from, to, tokenId);
     }
     
@@ -57,15 +85,14 @@ contract ERC9999 is IERC9999 , ERC721 {
         uint256 tokenId,
         bytes memory _data
     ) public virtual override {
-        require(_isApprovedOrUser(_msgSender(), tokenId), "ERC9999: transfer caller is not owner nor approved");
+        require(_isApprovedOrUser(_msgSender(), tokenId) || _isApprovedOrOwner(_msgSender(),tokenId), "ERC9999: transfer caller is not user or owner nor approved");
         _safeTransferUser(from, to, tokenId, _data);
     }
     
     function _isApprovedOrUser(address spender, uint256 tokenId) internal view virtual returns (bool) {
         require(ERC721._exists(tokenId), "ERC9999: operator query for nonexistent token");
         address user = ERC9999.userOf(tokenId);
-        address owner = super.ownerOf(tokenId);
-        return (spender == user || spender == owner);
+        return (spender == user || getApprovedUser(tokenId) == user);
     }
     
     function _safeTransferUser(
@@ -82,13 +109,13 @@ contract ERC9999 is IERC9999 , ERC721 {
         address to,
         uint256 tokenId
     ) internal virtual {
-        require(ERC9999.userOf(tokenId) == from, "ERC9999: transfer of token that is not use");
+        require(ERC9999.userOf(tokenId) == from || ERC721.ownerOf(tokenId) == from, "ERC9999: transfer of token that is not use");
         require(to != address(0), "ERC9999: transfer to the zero address");
 
         _beforeTokenTransferUser(from, to, tokenId);
 
         // Clear approvals from the previous owner
-        //_approve(address(0), tokenId);
+        _approveUser(address(0), tokenId);
 
         _balancesOfUser[from] -= 1;
         _balancesOfUser[to] += 1;
